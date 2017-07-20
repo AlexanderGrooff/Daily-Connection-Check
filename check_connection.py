572da5c5b2b18b0f5de260419db287f51b4b6adf
AlexanderGrooff <alexandergrooff@gmail.com>
2017-07-20 11:11:24 +0200

    Retrieved results from speedtest

--- check_connection.py
+++ check_connection.py
@@ -1,5 +1,7 @@
 import os
+import subprocess
 import argparse
+import re
 
 parser = argparse.ArgumentParser(description="Routine internet quality checker")
 parser.add_argument("-t", "--type", action="store", default="ping", help="Set to either ping or speedtest", choices=["ping", "speedtest"])
@@ -16,5 +18,9 @@ if args.type == "ping":
         print("Connection is down")
         os.system("echo \"Failure\" | mail -s \"Ping failure\" alexbotmailer@gmail.com")
 if args.type == "speedtest":
-    response = os.system("speedtest-cli --simple")
+    response = str(subprocess.check_output("speedtest-cli --simple", shell=True))
+    # 'Ping: 6.886 ms\r\nDownload: 10.96 Mbit/s\r\nUpload: 12.04 Mbit/s\r\n'
+    # re.S for . to also include newline chars
+    groups = re.search(r"Ping: (.*) ms.*Download: (.*) Mbit.*Upload: (.*) Mbit", response, re.S)
     print(response)
+    print(groups.group(1), groups.group(2), groups.group(3))
